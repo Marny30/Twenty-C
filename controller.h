1884d10ffd809258b2e8d236cfd076b7de41a442
Julien Marin <marin.julien@etu.umontpellier.fr>
2016-11-12 19:38:10 +0100

    rectifications header controller

--- controller.h
+++ controller.h
@@ -1,12 +1,17 @@
 #include "common.h"
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
+
+#define KEY_RIGHT
+#define KEY_LEFT
+#define KEY_DOWN
+#define KEY_UP
+#define KEY_SELECT
+
+struct Controller{
+  double waittime;          /* in ms */
+};
 
 int controller_main(GameModel *g);
 int get_key();
-void key_handling(GameModel *g);
+void key_handling(GameModel *g, int key);
 void switchmodeCursor(GameModel *g);
 void moveCursor(GameModel *g);
-
-Case selected();
